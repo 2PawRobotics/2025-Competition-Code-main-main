4c0a462d5f42cc3bf621d7d9ad8301e240b498a8
2PawRobotics <hsrobotics@ppps.org>
2025-03-21 18:14:02 -0400

    added delay to sensor

--- src/main/java/frc/robot/subsystems/EndEffectorSys.java
+++ src/main/java/frc/robot/subsystems/EndEffectorSys.java
@@ -8,11 +8,14 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANDevices;
 import com.revrobotics.ColorSensorV3;
 
 public class EndEffectorSys extends SubsystemBase {
+
+    private final Timer delay = new Timer();
     
     public static SparkMax ejectionMtr = new SparkMax(CANDevices.ejectionMtrId, MotorType.kBrushed);
     
@@ -38,8 +41,16 @@ public class EndEffectorSys extends SubsystemBase {
         if(DriverStation.isEnabled() == true){
 
             if (!photoSensor.get() == true){
-                System.out.println("Object Detected");
-                coralInSystem = true;
+                if (!coralInSystem) {
+                    coralInSystem = true;
+                    delay.reset();
+                    delay.start();
+                }
+                if (delay.hasElapsed(5)) {
+                    System.out.println("Object Detected");
+                    System.out.println("Delay");
+                    delay.stop();
+                }
             }
             else {
                 coralInSystem = false;
@@ -53,6 +64,6 @@ public class EndEffectorSys extends SubsystemBase {
             else {
                 ejectionMtr.set(0);
             }
-    }
+        }
     }
 }
